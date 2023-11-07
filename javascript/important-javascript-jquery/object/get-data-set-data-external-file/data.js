938f238628a9e260458c0bca5cad1be9dde28f48
noman.na <noman.na@I2A-PC-100>
2023-11-07 19:14:47 +0530

    new

--- javascript/important-javascript-jquery/object/get-data-set-data-external-file/data.js
+++ javascript/important-javascript-jquery/object/get-data-set-data-external-file/data.js
@@ -20,3 +20,25 @@ const data = [
         text: "<p class='text'>Text 5</p>"
     }
 ];
+const data1 = [
+    {
+        head: "Head 1",
+        text: "Text 1"
+    },
+    {
+        head: "Head 2",
+        text: "Text 2"
+    },
+    {
+        head: "Head 3",
+        text: "Text 3"
+    },
+    {
+        head: "Head 4",
+        text: "Text 4"
+    },
+    {
+        head: "Head 5",
+        text: "Text 5"
+    }
+];
