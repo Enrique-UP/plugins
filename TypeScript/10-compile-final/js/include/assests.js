1f851bf15c1da50dc7f3409a20bae410e6540bf7
noman.na <noman.na@I2A-PC-100>
2024-03-29 16:02:56 +0530

    new

--- TypeScript/10-compile-final/js/include/assests.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-document.write("Include Assests file");
--- javascript/important-javascript-jquery/image-light-box-js/images.js
+++
@@ -1,5 +0,0 @@
-var images= [
-  "../../../sliders/owl-carousel/owl-carosel-thumbs-with-scroll/images/1.jpg",
-  "../../../sliders/owl-carousel/owl-carosel-thumbs-with-scroll/images/2.jpg",
-  "../../../sliders/owl-carousel/owl-carosel-thumbs-with-scroll/images/3.jpg"
-];
