b2109b24b8686e5af29cf6f4a8e19620dba2597a
noman.na <noman.na@I2A-PC-100>
2024-04-23 18:25:52 +0530

    new

---
+++ TypeScript/10-compile-final/js/include/assests.js
@@ -0,0 +1,2 @@
+"use strict";
+document.write("Include Assests file");
---
+++ webpage-starting/starting-webpage-grid/js/main.js
@@ -0,0 +1,23 @@
+new WOW().init();
+$(".slider").owlCarousel({
+	autoplay:true,
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
+
+// for fw not spacing
+document.querySelectorAll("main .fw").forEach((e) => e.parentElement.classList.add("fwp"));
+// set margin-bottom of col parent col
+document.querySelectorAll(".row [class*=col-] [class*=col-]").forEach((e) => e.parentElement.closest("[class*=col-]").style.marginBottom = 0);
