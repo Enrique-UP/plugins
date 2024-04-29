f69253a17b4c07e4fb3c5d81a7a9cbe4b85161f2
noman.na <noman.na@I2A-PC-100>
2024-04-29 12:05:39 +0530

    .

---
+++ TypeScript/8-config-file/script.js
@@ -0,0 +1,7 @@
+document.write("tsc.cmd --init");
+document.write("<br>");
+document.write("tsc.cmd");
+document.write("<br>");
+document.write("Change compile version es5 or es6 etc.");
+document.write("<br>");
+document.write("Welcome");
