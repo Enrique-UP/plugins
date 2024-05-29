ef43baad6646cf8923407e587b7621a95f71513b
noman.na <noman.na@I2A-PC-100>
2024-05-29 18:46:12 +0530

    new

--- TypeScript/2/demo.js
+++
@@ -1,4 +0,0 @@
-console.log("Hello world");
-var a;
-a = 5;
-console.log(a);
--- TypeScript/5-tupleDataType/demo.js
+++
@@ -1,7 +0,0 @@
-var details = {
-    id: 1,
-    name: "Demo",
-    role: [1, "Admin"]
-    // role:["Admin"] //showing error
-    // role:[1] //showing error
-};
