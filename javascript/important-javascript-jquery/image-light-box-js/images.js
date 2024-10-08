14ea5ae0b146bae95a48dc09c0431aff6d97f7c1
noman.na <noman.na@I2A-PC-100>
2024-10-08 14:20:29 +0530

    new

--- javascript/important-javascript-jquery/image-light-box-js/images.js
+++ javascript/important-javascript-jquery/image-light-box-js/images.js
@@ -1,5 +1,5 @@
-var images= [
-  "../../../sliders/owl-carousel/owl-carosel-thumbs-with-scroll/images/1.jpg",
-  "../../../sliders/owl-carousel/owl-carosel-thumbs-with-scroll/images/2.jpg",
-  "../../../sliders/owl-carousel/owl-carosel-thumbs-with-scroll/images/3.jpg"
+var images = [
+  "../../../sliders/owl-carousel/images/1.jpg",
+  "../../../sliders/owl-carousel/images/2.jpg",
+  "../../../sliders/owl-carousel/images/3.jpg"
 ];
