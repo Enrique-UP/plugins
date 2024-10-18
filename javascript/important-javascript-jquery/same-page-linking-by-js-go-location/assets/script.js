4775b5865d5a096fead59bcfbbb3c0e5c47889fb
noman.na <noman.na@I2A-PC-100>
2024-10-18 18:49:45 +0530

    new

--- javascript/important-javascript-jquery/same-page-linking-by-js-go-location/assets/script.js
+++ javascript/important-javascript-jquery/same-page-linking-by-js-go-location/assets/script.js
@@ -1,4 +1,4 @@
-document.querySelectorAll("a").forEach((e) => {
+document.querySelectorAll("main header a").forEach((e) => {
     const href = e.href;
     if (href.includes("#")) {
         const valueAfterHash = href.substring(href.indexOf("#") + 1);
@@ -31,6 +31,30 @@ function scrollUp(e){
             top: top - headerHeight,
             behavior: 'smooth'
         });
-    }, 10);
-    
+    }, 25);
 }
+
+(function(events, index) {
+    events.forEach(event => window.addEventListener(event, index));
+})(["load", "scroll", "resize"], function(){
+    document.querySelectorAll("main header a").forEach(function(e) {
+        if (e.href === window.location.href) e.classList.add('active');
+        
+        e.addEventListener("click", function(){
+            document.querySelectorAll('main header a').forEach((e) => e.classList.remove("active"));
+            if (this.classList.contains('active')) this.classList.remove('active');
+            else this.classList.add('active');
+        });
+    });
+});
+
+
+// (function(events, index) {
+//     events.forEach(event => window.addEventListener(event, index));
+// })(["load", "scroll", "resize"], function(){
+//     document.querySelectorAll("section").forEach(function(e) {
+//         if (e.href === window.location.href) e.classList.add('active');
+        
+        
+//     });
+// });
