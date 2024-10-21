ce87b6dc4bcf9366009e04d2de7aa3fa35118b15
noman.na <noman.na@I2A-PC-100>
2024-10-21 17:54:13 +0530

    new

--- javascript/important-javascript-jquery/same-page-linking-by-js-go-location/assets/script.js
+++ javascript/important-javascript-jquery/same-page-linking-by-js-go-location/assets/script.js
@@ -53,8 +53,38 @@ function scrollUp(e){
 //     events.forEach(event => window.addEventListener(event, index));
 // })(["load", "scroll", "resize"], function(){
 //     document.querySelectorAll("section").forEach(function(e) {
-//         if (e.href === window.location.href) e.classList.add('active');
-        
-        
+//         if (e.id === "#" + aHref(index)) e.classList.add('active');        
 //     });
 // });
+// function aHref(f){
+//     document.querySelectorAll("main header a").forEach(function(e) {
+//         f = e.href;
+//     });
+// }
+
+
+
+// (function(events) {
+//     events.forEach(event => window.addEventListener(event, function() {
+//         document.querySelectorAll("section").forEach(function(section) {
+//             // Iterate through all 'a' elements in the header
+//             document.querySelectorAll("main header a").forEach(function(anchor) {
+//                 if(anchor.getAttribute("data-link")){
+//                     // If the section's ID matches the href ID, add 'active', else remove it
+//                     if (section.id === anchor.getAttribute("data-link").substring(1)) {
+//                         // document.querySelectorAll("#" + hrefId).forEach(function(f) {
+//                         //     f.classList.add("active");
+//                         // });
+//                         section.querySelectorAll(section.getAttribute("id")).forEach(function(f){
+//                             f.classList.add("active");
+//                         });
+//                     } else {
+//                         section.querySelectorAll(section.getAttribute("id")).forEach(function(f){
+//                             f.classList.remove("active");
+//                         });
+//                     }
+//                 }
+//             });
+//         });
+//     }));
+// })(["load", "scroll", "resize"]);
