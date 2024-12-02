da9e024956eb1c3bcac549a6c3745d73ab0e624c
noman.na <noman.na@I2A-PC-100>
2024-12-02 15:05:49 +0530

    new

--- javascript/important-javascript-jquery/working-but-not-proper/ig-masonry-data-load-lazyLoad-searchBar-filterTab-imageAdd0-infinite/js/data.js
+++ javascript/important-javascript-jquery/working-but-not-proper/ig-masonry-data-load-lazyLoad-searchBar-filterTab-imageAdd0-infinite/js/data.js
@@ -4,7 +4,21 @@ const tags = [
     {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}
 ];
 tags.forEach(function(e) {
-  //content += `<div class="item"><div class="content"><h3>${e.text}</h3><img src=${"./images/"+e.img+".jpg"} /></div><!--content--></div><!--item-->`;
-  content += `<div class="item"><div class="content"><h3></h3><img class="lazy" /></div><!--content--></div><!--item-->`;
+  // content += `
+  //   <div class="item">
+  //     <div class="content">
+  //       <h3>${e.text}</h3>
+  //       <img src=${"./images/"+e.img+".jpg"} />
+  //     </div><!--content-->
+  //   </div><!--item-->
+  // `;
+  content += `
+    <div class="item">
+      <div class="content">
+        <h3></h3>
+        <img class="lazy" />
+      </div><!--content-->
+    </div><!--item-->
+  `;
 });
 document.querySelector("div.grid").innerHTML = content;
