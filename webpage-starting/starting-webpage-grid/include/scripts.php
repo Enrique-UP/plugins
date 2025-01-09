a781bbac0a85e37da7e6e5ca56fedf6b3378214f
noman.na <noman.na@I2A-PC-100>
2025-01-09 19:27:21 +0530

    new

--- webpage-starting/starting-webpage-grid/include/scripts.php
+++ webpage-starting/starting-webpage-grid/include/scripts.php
@@ -1,4 +1,9 @@
 <i class="icon scrollTop">&#xa022;</i>
 <script src="./assets/js/jquery.min.js"></script>
 <script src="./assets/js/wow.min.js"></script>
-<script src="./assets/js/main.js"></script>
+<script type="text/javascript" src="./assets/js/main.js"></script>
+<script type="text/javascript">
+  (function(){
+  	console.log("Lorem");
+  });
+</script>
