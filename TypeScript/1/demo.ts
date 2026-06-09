929ccce9a3201a03dd33b0e6329f39d936a1883d
nomaanarif786 <your-email@example.com>
2026-06-09 14:14:45 +0530

    new

--- TypeScript/1/demo.ts
+++ TypeScript/1/demo.ts
@@ -1,6 +1,6 @@
 let b:number;
 b = 3;
-// b = "Demo"; // showing error the type of b is number
+// b = "WsCube Tech vidoes"; // showing error the type of b is number
 // b = false; // showing error the type of b is number
 // b = { // showing error the type of b is number
 //     name: "Demo1",
