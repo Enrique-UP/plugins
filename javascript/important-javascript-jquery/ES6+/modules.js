b30fc533ea2351c58b680f6fd6a85645889fd5cb
nomaanarif786 <your-email@example.com>
2026-06-30 19:16:18 +0530

    new

---
+++ javascript/important-javascript-jquery/ES6+/modules.js
@@ -0,0 +1,23 @@
+export const name = "Enrique";
+export const age = 33;
+
+export function add(a, b){
+    return a + b;
+}
+
+const details = () => {
+  const name = "Jesse";
+  const age = 40;
+  return name + ' is ' + age + ' years old.';
+};
+export default details;
+
+
+
+export const PI = 3.14159;
+export function adds(a, b) {
+  return a + b;
+}
+export function multiply(a, b) {
+  return a * b;
+}
---
+++ javascript/important-javascript-jquery/ES6+/modules0.js
@@ -0,0 +1,7 @@
+export const name = "Nomaan";
+
+export const age = 34;
+
+export function hello() {
+    console.log("Hello");
+}
