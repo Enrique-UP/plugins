ad4a2fca0641065ef3dfcdaa8e75cb4e5b2187e6
nomaanarif786 <your-email@example.com>
2026-08-13 18:49:28 +0530

    new

--- TypeScript/TYPESCRIPT----------JS----------Practice/js/scripts/script.js
+++ TypeScript/TYPESCRIPT----------JS----------Practice/js/scripts/script.js
@@ -1,25 +1,14 @@
 "use strict";
 (function () {
-    const details = [];
-    document.querySelectorAll("ul > li").forEach((e) => {
-        details.push({
-            data: e.getAttribute("data-text"),
-            id: e.getAttribute("id"),
-            text: e.innerHTML.trim()
-        });
-    });
-    console.log(details);
-    const removeDuplicates = [...new Map(details.map(item => [item.text, item])).values()];
-    console.log(removeDuplicates);
-    console.log(JSON.stringify(removeDuplicates));
-    document.querySelector("ul").insertAdjacentHTML("afterend", "<ol></ol>");
-    removeDuplicates.map((e, i) => {
-        document.querySelector("ol").innerHTML += `<li data-tt=${e.data} id=${e.id}>${e.text}</li>`;
-    });
-    removeDuplicates.forEach(function (e, i) {
-        localStorage.setItem(`id ${i}`, e.id);
-        localStorage.setItem(`Data ${i}`, e.data);
-        localStorage.setItem(`text ${i}`, e.text);
-    });
+    const obj1 = {
+        name: "Enrique",
+        age: 40,
+        place: "Delhi",
+        full: function (street, state) {
+            return `Hello! I am ${this.name} from ${this.place}, ${street}-${state} & ${this.age} years old.`;
+        },
+        s:null
+    };
+    localStorage.setItem("Details", JSON.stringify(obj1));
 })();
 //# sourceMappingURL=script.js.map
